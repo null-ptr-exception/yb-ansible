3df9cf2ac32210c1644d7b11f3ff4c5c3db8512c
張旭 <zx1986@gmail.com>
2026-06-08 23:02:07 +0800

    test(molecule): use consistent stream8 ssh user

--- tests/test_rhel8_yb_build_config.sh
+++ tests/test_rhel8_yb_build_config.sh
@@ -44,9 +44,12 @@ assert_contains .github/workflows/build-shipper.yml 'tags: \${{ env\.REGISTRY }}
 # shellcheck disable=SC2016
 assert_contains shipper/build.sh 'IMAGE="\${3:-yb-shipper:\${YB_VERSION}-\${YB_BUILD}}"'
 assert_contains molecule/default/create.yml 'CentOS-Stream-GenericCloud-8-latest\.x86_64\.qcow2'
-assert_contains molecule/default/create.yml "'user': 'cloud-user'"
+assert_contains molecule/default/create.yml '^[[:space:]]*molecule_ssh_user:[[:space:]]+cloud-user[[:space:]]*$'
+assert_contains molecule/default/create.yml "'user': molecule_ssh_user"
+assert_contains molecule/default/create.yml '{{ molecule_ssh_user }}@{{ item\.address }}'
+assert_contains molecule/default/create.yml 'ansible_user={{ molecule_ssh_user }}'
 assert_contains molecule/default/tasks/create_vm.yml '--os-variant (centos-stream8|rhel8\.[0-9]+|rhel8-unknown)'
-assert_contains molecule/default/tasks/create_vm.yml '^[[:space:]]*-[[:space:]]*name:[[:space:]]+cloud-user[[:space:]]*$'
+assert_contains molecule/default/tasks/create_vm.yml '^[[:space:]]*-[[:space:]]*name:[[:space:]]+"{{ molecule_ssh_user }}"[[:space:]]*$'
 assert_contains molecule/default/verify.yml '^[[:space:]]*yb_shipper_tag:[[:space:]]*"2025\.2\.3\.2-b1"[[:space:]]*$'
 
 assert_not_contains README.md 'CentOS 7|RHEL 7'
